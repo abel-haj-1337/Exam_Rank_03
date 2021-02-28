a8876378f6057c123e5e589a6279ede3a91c4ebe
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-02-28 10:02:42 +0100

    organized things

--- micro_paint.h
+++ micro_paint.h
@@ -1,1 +1,32 @@
+#ifndef MICRO_PAINT_H
 
+# define MICRO_PAINT_H
+
+// # include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+// # include <string.h>
+# include <unistd.h>
+
+typedef struct	s_conf
+{
+	int	width;
+	int	height;
+	char	bg;
+}				t_conf;
+
+typedef struct	s_rect
+{
+	char	type;
+	int	x;
+	int	y;
+	int	width;
+	int	height;
+	char	bg;
+}				t_rect;
+
+int				ft_strlen(char *str);
+void			ft_putchar(char c);
+void			ft_putstr(char *str);
+
+#endif
--- utils.c
+++ utils.c
@@ -1,1 +1,30 @@
+#include "micro_paint.h"
 
+int		ft_strlen(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
