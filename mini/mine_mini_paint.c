1355c94203e9dbb2254216c882ff8534df9fdb6d
Abdelaziz Bel haj <abel-haj@e1r2p8.1337.ma>
2021-03-02 17:07:45 +0100

    completed the function

--- mini/mine_mini_paint.c
+++ mini/mine_mini_paint.c
@@ -2,19 +2,18 @@
 
 char	**ft_init_map(int w, int h, char c)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	**m;
 
 	m = malloc(h * sizeof(char *));
-
 	if (m)
 	{
 		i = 0;
 		while (i < h)
 		{
 			j = 0;
-			m[i] = malloc(w * sizeof(char));
+			m[i] = malloc(w * sizeof(char) + 1);
 			while (j < w)
 			{
 				m[i][j] = c;
@@ -24,8 +23,59 @@ char	**ft_init_map(int w, int h, char c)
 			i++;
 		}
 	}
-
 	return (m);
+}
+
+void	ft_draw_in_map(char ***m, t_conf co, t_circle ci)
+{
+	int		i;
+	int		j;
+	float	d;
+
+	i = 0;
+	while (i < co.height)
+	{
+		j = 0;
+		while (j < co.width)
+		{
+			// calculate distance between current position and circle center
+			d = sqrt((j - ci.x) * (j - ci.x) + (i - ci.y) * (i - ci.y));
+			if (d < 0)
+				d *= -1;
+
+			// if distance is equal or lower to circle center
+			if (d <= ci.radius)
+			{
+				// if circle is not empty
+				// or
+				// current position is at the border of the circle
+				if (ci.type == 'C' || floor(d) == ci.radius
+					|| ceil(d) == ci.radius)
+					(*m)[i][j] = ci.bg;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_print_map(char **m, int w, int h)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < h)
+	{
+		j = 0;
+		while (j < w)
+		{
+			ft_putchar(m[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
 }
 
 void	ft_free_map(char ***m, int h)
@@ -41,53 +91,13 @@ void	ft_free_map(char ***m, int h)
 	free(*m);
 }
 
-void	ft_print_map(char **m, int w, int h)
+int	main(int argc, char *argv[])
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < h)
-	{
-		j = 0;
-		while (j < w)
-		{
-			printf("%c", m[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
-}
-
-void	ft_draw_in_map(char ***m, t_conf co, t_circle ci)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i)
-	{
-		j = 0;
-		while (j)
-		{
-			j++;
-		}
-		i++;
-	}
-}
-
-// TODO
-	// replace printf with putchar/str
-	// remove //debbuging
-	// draw in map
-int		main(int argc, char *argv[])
-{
-	int	ret;
-	t_conf	co;
+	int			ret;
+	t_conf		co;
 	t_circle	ci;
-	FILE	*file;
-	char	**map;
+	FILE		*file;
+	char		**map;
 
 	if (argc == 2)
 	{
@@ -101,10 +111,6 @@ int		main(int argc, char *argv[])
 		// read map configuration
 		ret = fscanf(file, "%d %d %c\n", &co.width, &co.height, &co.bg);
 
-		// debugging
-			printf("%d: %d %d %c\n",
-				ret, co.width, co.height, co.bg);
-
 		if (ret != 3 || co.width <= 0 || co.width > 300
 			|| co.height <= 0 || co.height > 300)
 		{
@@ -114,35 +120,35 @@ int		main(int argc, char *argv[])
 
 		// init map with given configuration
 		map = ft_init_map(co.width, co.height, co.bg);
-		// print map
-		ft_print_map(map, co.width, co.height);
 
-		// read circles dimensions
-		while ((ret = fscanf(file, "%c %f %f %f %c\n",
-				&ci.type, &ci.x, &ci.y, &ci.radius, &ci.bg)) > 0)
+		while (ret > 0)
 		{
+			// read circles dimensions
+			ret = fscanf(file, "%c %f %f %f %c\n",
+					&ci.type, &ci.x, &ci.y, &ci.radius, &ci.bg);
+
 			// all 5 dimensions are given
 			if (ret == 5)
 			{
-
-				// debuggin
-				printf("%d: %c %f %f %f %c\n",
-					ret, ci.type, ci.x, ci.y, ci.radius, ci.bg);
-
-				// draw in map
+				if (ci.x < 0 || ci.y < 0 || ci.radius <= 0
+					|| (ci.type != 'c' && ci.type != 'C'))
+				{
+					ft_free_map(&map, co.height);
+					ft_putstr("Error: Operation file corrupted\n");
+					return (1);
+				}
 				ft_draw_in_map(&map, co, ci);
 			}
-			else
+			else if (ret != -1)
 			{
 				ft_putstr("Error: Operation file corrupted\n");
 				return (1);
 			}
 		}
 
-		// debugging
-		printf("ret %d\n", ret);
+		ft_print_map(map, co.width, co.height);
 
-		// prevent leaks
+		// no leaks tolarated
 		ft_free_map(&map, co.height);
 	}
 	else
